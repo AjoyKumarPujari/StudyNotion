f4a585611ac163ba0826aa0ac63cfd88a64efb7a
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-08-15 12:02:12 +0530

    Became an Instructr

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,9 @@ import Banner from '../assets/Images/banner.mp4'
 import CodeBlocks from '../components/core/HomePage/CodeBlocks'
 import TimelineSection from '../components/core/HomePage/TimelineSection'
 import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+
 const Home = () => {
   return (
     <div>
@@ -180,8 +183,17 @@ const Home = () => {
 
         {/*section3 */}  
 
+        <div className='w-11/12 mx-auto max-w-maxContent flex-col items-center justify-center justify-between gap-8 
+        first-letter bg-richblack-900 text-white'>
+              <InstructorSection/>
+              <h2 className='text-center text-4xl font-semibold mt-10 '>Review from Other Learners</h2>
+              {/*Review slider here */}
+              {/* <ReviewSlider/> */}
+        </div>
+
 
         {/*footer */}
+        <Footer/>
     </div>
   )
 }
