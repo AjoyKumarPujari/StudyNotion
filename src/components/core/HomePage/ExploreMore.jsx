e062879819b16256866c239ed625faed4628cd2a
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-09-02 22:39:29 +0530

    Explore more tab added

--- src/components/core/HomePage/ExploreMore.jsx
+++ src/components/core/HomePage/ExploreMore.jsx
@@ -24,8 +24,30 @@ const tabName = [
     <div>
         <div className='text-4xl font-semibold text-center'>
         Unlock the
-        <Highlighttext text={"  Power of Code"}/>
-           
+        <Highlighttext text={"  Power of Code"}/> 
+        </div>
+        
+        <p className='text-center text-richblack-300 text-ssm  mt-3'>
+            Learn to build anything you can imagine
+        </p>
+        
+        <div className='flex flex-row rounded-full bg-richblack-800 mb-5 px-1 gap-2 py-1 boarder-richblack-100 mt-5'>
+            {
+                tabName.map((element, index)=>{
+                    return (
+                    <div
+                        className={`text-[16px] flex flex-row items-center gap-2
+                        ${currentTab === element ? "bg-richblack-900 text-richblack-5 font-medium"
+                            :" text-richblack-200"} rounded-full transation-all duration-200 cursor-pointer
+                            hover:bg-richblack-900 hover:text-richblack-5 px-7 py-2`}
+                            key={index}
+                            onClick={()=> setMyCards(element)}
+                            >
+                            {element}
+                        </div>
+                    )
+                })
+            }
         </div>
 
     </div>
